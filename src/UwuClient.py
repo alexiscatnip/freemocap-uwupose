2f93888ec0dc1dc2bfcd50380f314a16e1606a3f
alexiscatnip <alexiscatnip@gmail.com>
2022-08-06 22:46:19 +0800

    refactor: make client use asyncio, socketio

--- src/UwuClient.py
+++ src/UwuClient.py
@@ -1,25 +1,63 @@
-import uvicorn
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
+import asyncio
+import threading
+from collections import deque
 
-app = FastAPI()
+import socketio
+
+import freemocap
+from freemocap import inference_gui, session
+from src import steamVR_thread
+
+sio = socketio.AsyncClient()
 
 """
-define client's API.
+client, subscribes to a topic published by the server.
 """
 
-def test_websocket():
-    client = TestClient(app)
-    with client.websocket_connect("/pose") as websocket:
-        data = {"Hello, the server is up."}
-        websocket.send_json(data);
+PORT = int("8000")
+network_output_thread = None
+pipe_3d_points_in = deque(maxlen=1)
 
-def test_read_main():
-    client = TestClient(app)
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello, the server is up."}
 
-if __name__ == "__main__":
-    test_read_main()
-    test_websocket()
+@sio.event
+async def connect():
+    print('connection established')
+
+@sio.event
+async def my_message(data):
+    print('message received with ', data)
+    pump_data_to_SteamVR_Thread(data)
+
+@sio.event
+async def disconnect():
+    print('disconnected from server')
+
+
+def pump_data_to_SteamVR_Thread(data):
+    # change(data)
+
+
+    pipe_3d_points_in.append(data)
+
+
+async def main():
+    sesh = session.Session()
+
+    pipe_HMD_out = deque(maxlen=1)
+    network_output_thread = steamVR_thread.SteamVRThread(
+        pipe_3d_points_in,
+        pipe_HMD_out,
+        sesh
+    )
+
+    # start UI thread
+    gui_thread = threading.Thread(target=inference_gui.make_inference_gui,
+                                  args=(session,),
+                                  daemon=True)
+    gui_thread.start()
+
+    print ("now waiting for packets from port 5000")
+    await sio.connect('http://localhost:5000')
+    await sio.wait()
+
+asyncio.run(main())
